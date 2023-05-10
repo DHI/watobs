13d9032ce03115a8e83fab06ec63bb44c4458b6a
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2023-05-10 13:45:36 +0200

    requires_DATAFARM_API_KEY

--- tests/test_datafarm.py
+++ tests/test_datafarm.py
@@ -3,15 +3,22 @@ import pytest
 from watobs.datafarm import DatafarmRepository
 
 
+def requires_DATAFARM_API_KEY():
+    api_key = os.environ.get("DATAFARM_API_KEY")
+    reason = "Environment variable DATAFARM_API_KEY not present"
+    return pytest.mark.skipif(api_key is None, reason=reason)
+
+
 @pytest.fixture
 def repo():
-    api_key = os.getenv("DATAFARM_API_KEY")
+    api_key = os.environ.get("DATAFARM_API_KEY")
     assert api_key is not None
     dfr = DatafarmRepository(api_key)
     dfr.connect()
     return dfr
 
 
+@requires_DATAFARM_API_KEY()
 def test_list_time_series(repo):
     assert repo.access_token is not None
     assert repo.list_time_series() is not None
