a4750db2c63f092b67b0714ca3dee58af3c26418
Henrik Andersson <jan@dhigroup.com>
2024-10-30 10:29:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="watobs",
-    version="0.2.dev2",
+    version="0.1.1",
     install_requires=["numpy", "pandas", "requests", "matplotlib"],
     extras_require={
         "dev": [
